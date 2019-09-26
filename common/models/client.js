46d64dd51417793f75035dc0d390684470b21496
hadifikri <hadifikri@gmail.com>
2019-09-26 17:23:40 +0800

    up to remote hooks

--- common/models/client.js
+++ common/models/client.js
@@ -2,6 +2,28 @@
 /* eslint-disable */
 module.exports = function(Client) {
     disableUnusedRemotes(Client);
+
+    var app;
+    Client.on('attached',a=>{
+        app = a;
+    });
+    Client.beforeRemote("create",function(ctx,model,next){
+        console.log("before remote create (ctx.args.data)", ctx.args.data);
+        var userinfo = ctx.args.data;
+        if(userinfo.role){
+            sanitizeRegistration(app,ctx,next);
+        }else{
+            next({
+                "name":"Bad Request",
+                "status":"404",
+                "message":"Please provide the user role information"
+            });
+        }
+    });
+    Client.afterRemote("create", function(ctx,model,next){
+        console.log("after remote create :",model);
+        ctx.args.data.role.principals.create({principalType:"User",principalId:model.id},next);
+    });
 };
 
 function disableUnusedRemotes(Client){
@@ -37,4 +59,36 @@ function disableUnusedRemotes(Client){
     Client.disableRemoteMethodByName('prototype.__findById__accessTokens');
     Client.disableRemoteMethodByName('prototype.__get__accessTokens');
     Client.disableRemoteMethodByName('prototype.__updateById__accessTokens');
+}
+
+
+//========================================================================
+async function sanitizeRegistration(app,ctx,next){
+    var appliedRole = ctx.args.data.role;
+    try{
+        var role = await _findOne(app.models.Role,{where:{name:appliedRole}});
+        console.log("Role found :",role);
+        if(role){
+            ctx.args.data.role = role;
+            next();
+        }else{
+            next({
+                "name":"Role not found",
+                "status":404,
+                "message":"The requested role do not exist"
+            });
+        }
+    }catch(err){
+        next(err);
+    }
+}
+
+//================ Promises ==============================================
+function _findOne(model,filter){
+    return new Promise((resolve,reject)=>{
+        model.findOne(filter,(err,res)=>{
+            if(err){return reject(err)}
+            resolve(res);
+        })
+    })
 }
